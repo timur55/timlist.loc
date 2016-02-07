814c799be331ac546e447dff0a34a9db2b45eb6f
timur <dd@sdsd.sd>
2016-02-07 17:57:19 +0300

    Rejected commit: Default (2)

---
+++ index.php
@@ -0,0 +1,40 @@
+<?
+
+
+function a (){
+
+    echo "Фунция A";
+
+}
+
+define("VAR", "Hello world.");
+$var = "123";
+
+class B {
+
+    public function c(){
+
+       // return a();
+
+    }
+
+    public function get_c(){
+
+        //echo $var;
+
+    }
+
+}
+
+$b = new B;
+
+
+
+echo "Hello dsfsds 121212 dfddfdfddf dfdfdf dfdfdf dfdfdfdf";
+
+
+
+
+
+
+?>
